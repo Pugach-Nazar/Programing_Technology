91dc93023381abfe4ec71a9039ea69583341e15c
Nazar Pugach <qwertyasd0933@gmail.com>
2024-11-18 14:23:42 +0200

    commit

--- terraform/main.tf
+++ terraform/main.tf
@@ -19,33 +19,33 @@ provider "aws" {
   region     = "us-east-1"
 }
 
-# resource "aws_security_group" "web_app" {
-#   name        = "web_app"
-#   description = "security group"
-#   ingress {
-#     from_port   = 80
-#     to_port     = 80
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+resource "aws_security_group" "web_app" {
+  name        = "web_app"
+  description = "security group"
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#  ingress {
-#     from_port   = 22
-#     to_port     = 22
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
-#   egress {
-#     from_port   = 0
-#     to_port     = 65535
-#     protocol    = "tcp"
-#     cidr_blocks = ["0.0.0.0/0"]
-#   }
+ ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
 
-#   tags= {
-#     Name = "web_app"
-#   }
-# }
+  tags= {
+    Name = "web_app"
+  }
+}
 
 resource "aws_instance" "webapp_instance" {
   ami           = "ami-0866a3c8686eaeeba"
